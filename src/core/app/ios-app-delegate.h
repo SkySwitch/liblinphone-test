BelledonneCommunications/liblinphone

Enable auto register push notification

src/core/app/ios-app-delegate.h
@@ -18,17 +18,20 @@
 */
 
 #include <UIKit/UIKit.h>
+#import <PushKit/PushKit.h>
 #include "private.h"
 #import <UserNotifications/UserNotifications.h>
 
-@interface IosAppDelegate : NSObject {
+@interface IosAppDelegate : NSObject <PKPushRegistryDelegate> {
 	std::shared_ptr<LinphonePrivate::Core> pcore;
-	NSTimer* mIterateTimer;
+	PKPushRegistry* voipRegistry;
 }
 
 - (void)setCore:(std::shared_ptr<LinphonePrivate::Core>)core;
 - (void)onLinphoneCoreStart;
 - (void)onLinphoneCoreStop;
 - (void)reloadDeviceOnRouteChangeCallback: (NSNotification *) notif;
+- (void)configure:(std::shared_ptr<LinphonePrivate::Core>)core;
+- (void)didRegisterForRemotePush:(NSData *)token;
 
 @end

SkySwitch/liblinphone-test

src/core/app/ios-app-delegate.h
@@ -18,6 +18,7 @@
 */
 
 #include <UIKit/UIKit.h>
+#import <PushKit/PushKit.h>
 #include "private.h"
 #import <UserNotifications/UserNotifications.h>
 
@@ -38,9 +39,13 @@
  iteration
  Its lifecicle is the same as the one from linphonecore init to destroy.
  */
-@interface IosAppDelegate : IosObject
+@interface IosAppDelegate : IosObject <PKPushRegistryDelegate> {
+	PKPushRegistry* voipRegistry;
+}
 
 - (id)initWithCore:(std::shared_ptr<LinphonePrivate::Core>)core;
+- (void)configure:(std::shared_ptr<LinphonePrivate::Core>)core;
+- (void)didRegisterForRemotePush:(NSData *)token;
 
 @end
 
@@ -52,6 +57,8 @@
 @interface IosHandler : IosObject
 
 - (id)initWithCore:(std::shared_ptr<LinphonePrivate::Core>)core;
+
 - (void)reloadDeviceOnRouteChangeCallback: (NSNotification *) notif;
+
 
 @end